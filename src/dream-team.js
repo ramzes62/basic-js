b4e1f5c28b59f14f9ec36569fbaa0e6b293e71fe
MrBiscuit <ramzes62@gmail.com>
2020-09-15 00:43:26 +0300

    feat: solve third dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false;
+  let dreamTeam = "";
+  members = members.filter((val) => (typeof val === "string"));
+  members = members.map((val)=>val.trim());
+  members.sort((a,b)=>a[0].toUpperCase()>b[0].toUpperCase()?1:-1);
+  members.forEach((val)=> dreamTeam += val[0].toUpperCase());
+  return dreamTeam;
 };
